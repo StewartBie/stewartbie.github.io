b1e9c4e19a841d5b7f030d686210a206218108ae
Ziqing Bie <bzqstewart@outlook.com>
2025-12-01 19:57:57 +0800

    change

---
+++ blog/2025-12-01-zephyr-blog.md
@@ -0,0 +1,14 @@
+---
+slug: zephyr-blog
+title: Zephyt Blog 
+authors: [zephyr]
+tags: [docusaurus]
+---
+
+# Deploy blog on my PC
+
+1. install with   `npm` , and then config and install
+
+Refer to the [docs](https://docusaurus.io/docs/deployment#deploying-to-github-pages). Add GitHub username and project name.
+
+Set `USE_SSH = true`, `deploymentBranch: 'gh-pages',`, and add a branch named gh-pages for remote repo is also needed.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,14 +23,16 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'StewartBie', // Usually your GitHub org/user name.
+  projectName: 'stewartbie.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash:true,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -50,7 +52,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-          'github.com/StewartBie/stewartbie.github.io',
+          'github.com/StewartBie/stewartbie.github.io/docs',
         },
         blog: {
           showReadingTime: true,
@@ -61,7 +63,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-          'github.com/StewartBie/stewartbie.github.io',
+          'github.com/StewartBie/stewartbie.github.io/blog',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
